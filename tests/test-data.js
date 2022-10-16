00e81682b8c5d869f35c2d43e2fba5d2ee24d047
Kathy Ye <kathyye15@gmail.com>
2022-10-15 17:07:36 -0700

    Updated price functions and added test-data to tests

---
+++ tests/test-data.js
@@ -0,0 +1,67 @@
+const receipt = [
+  {
+    item: "STRAINER",
+    price: 4.99
+  },
+  {
+    item: "RIBEYE",
+    price: 23.44
+  },
+  {
+    item: "FINE PEPPER POWDER",
+    price: 4.99
+  },
+  {
+    item: "RED PEPPER PASTE",
+    price: 5.99
+  },
+  {
+    item: "TOFU",
+    price: 1.29
+  },
+  {
+    item: "BONITO",
+    price: 9.99
+  },
+  {
+    item: "YELLOW DRAGON FRUIT",
+    price: 4.07
+  },
+  {
+    item: "GREEN ONION",
+    price: 0.33
+  },
+  {
+    item: "GROCERY",
+    price: 13.99
+  },
+  {
+    item: "ENOKI MUSHROOM",
+    price: 5.98
+  },
+  {
+    item: "KELP",
+    price: 3.99
+  },
+  {
+    item: "THAI TEA MIX",
+    price: 6.49
+  },
+  {
+    item: "SOY MILK",
+    price: 4.99
+  },
+  {
+    item: "BAG",
+    price: 0.25
+  },
+  {
+    subtotal: 90.78
+  },
+  {
+    tax: 0.43
+  },
+  {
+    total: 491.21
+  }
+]
