c9de9d5d60413d4970241d597dca2e0270583652
Cosze <codyszet@gmail.com>
2023-03-25 18:08:41 -0700

    Add home page

---
+++ pages/home.js
@@ -0,0 +1,11 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <main id="past-bills">
+      <Head>
+        <title>Receipt Splitter</title>
+      </Head>
+    </main>
+  )
+}
