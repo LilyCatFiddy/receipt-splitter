418881748c5476147cd66beb54725a54c693ac07
Cosze <codyszet@gmail.com>
2023-03-27 17:28:51 -0700

    Complete styling for tabs and added anonymous-person-icon

--- components/pastBills/body/emptyPill.jsx
+++ components/pastBills/body/emptyPill.jsx
@@ -4,7 +4,7 @@ import Pill from './pill.jsx';
 export default function EmptyPill() {
   return (
     <Pill classes='pastBills__pill--empty'>
-      <div className="icons__plus">
+      <div className="icon__plus">
         <img src="/plus-icon.svg" />
       </div>
     </Pill>
--- components/pastBills/footer/footer.jsx
+++ components/pastBills/footer/footer.jsx
@@ -4,8 +4,13 @@ import Tab from './tab.jsx';
 export default function Footer() {
   return (
     <div className="pastBills__tabList">
+      <Tab>Split Tab</Tab>
       <Tab>IOU</Tab>
-      <Tab>IOU</Tab>
+      <Tab>
+        <div className="icon__anonymous-person">
+          <img src="/anonymous-person-icon.svg" alt="Settings icon" />
+        </div>
+      </Tab>
     </div>
   );
 }
